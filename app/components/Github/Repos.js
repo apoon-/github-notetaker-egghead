83f145446a728836dda641f1c791db9a7f69f758
Anson Poon <anson.os.poon@gmail.com>
2015-12-27 00:33:02 -0500

    fin tutorial

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -1,18 +1,24 @@
-var React = require('react');
+import React from 'react';
 
-var Repos = React.createClass({
-  propTypes: {
-    username: React.PropTypes.string.isRequired,
-    repos: React.PropTypes.array.isRequired
-  },
-  render: function(){
-    return (
-      <div> 
-        <p> REPOS </p>
-        REPOS: {this.props.repos}
-      </div>
-    )
-  }
-});
+const Repos = ({repos}) => {
+  return (
+    <div> 
+      <h3> User Repos </h3>
+      <ul className="list-group">
+        {repos.map((repo, index) => (
+            <li className="list-group-item" key={index}>
+              {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
+              {repo.description && <p> {repo.description}</p>}
+            </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
-module.exports = Repos;
+Repos.propTypes = {
+  username: React.PropTypes.string.isRequired,
+  repos: React.PropTypes.array.isRequired
+};
+
+export default Repos;
--- app/components/SearchGithub.js
+++ app/components/SearchGithub.js
@@ -1,22 +1,21 @@
-var React = require('react');
+import React from 'react';
 var Router = require('react-router');
 
-var SearchGithub = React.createClass({
-  mixins: [Router.History],
-  getRef: function(ref){
-    this.usernameRef = ref;
-  },
-  handleSubmit: function(){
-    var username = this.usernameRef.value;
+class SearchGithub extends React.Component {
+  handleSubmit(){
+    const username = this.usernameRef.value;
     this.usernameRef.value = '';
-    this.history.pushState(null, "profile/" + username);
-  },
-  render: function(){
+    this.props.history.pushState(null, "/profile/" + username);
+  }
+  getRef(ref){
+    this.usernameRef = ref;
+  }
+  render(){
     return (
       <div className="col-sm-12">
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={() => this.handleSubmit()}>
           <div className="form-group col-sm-7">
-            <input type="text" className="form-control" ref={this.getRef}/>
+            <input type="text" className="form-control" ref={(ref) => this.getRef(ref)}/>
           </div>
           <div className="form-group col-sm-5">
             <button type="submit" className="btn btn-block btn-primary">
@@ -27,6 +26,10 @@ var SearchGithub = React.createClass({
       </div>
     )
   }
-});
+}
 
-module.exports = SearchGithub;
+SearchGithub.propTypes = {
+  history: React.PropTypes.object.isRequired
+}
+
+export default SearchGithub;

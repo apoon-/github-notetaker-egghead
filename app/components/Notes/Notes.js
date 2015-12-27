f6f1372b13f493e0b0d579714bb7a0750038e3a1
Anson Poon <anson.os.poon@gmail.com>
2015-12-27 00:40:31 -0500

    changed return syntax

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -2,15 +2,13 @@ import React from 'react';
 import NotesList from './NotesList';
 import AddNote from './AddNote';
 
-const Notes = ({username, notes, addNote}) => {
-  return (
-    <div>
-      <h3> Notes for {username} </h3>
-      <AddNote username={username} addNote={addNote} />
-      <NotesList notes={notes} />
-    </div>
-  )
-}
+const Notes = ({username, notes, addNote}) => (
+  <div>
+    <h3> Notes for {username} </h3>
+    <AddNote username={username} addNote={addNote} />
+    <NotesList notes={notes} />
+  </div>
+)
 
 Notes.propTypes = {
   username: React.PropTypes.string.isRequired,
--- app/components/Notes/NotesList.js
+++ app/components/Notes/NotesList.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
-const NotesList = ({notes}) => {
-  return (
-    <ul className="list-group">
-      {notes.map((note, index) => (
-        <li className="list-group-item" key={index}>{note}</li>
-      ))}
-    </ul>
-  )
-}
+const NotesList = ({notes}) => (
+  <ul className="list-group">
+    {notes.map((note, index) => (
+      <li className="list-group-item" key={index}>{note}</li>
+    ))}
+  </ul>
+)
 
 NotesList.propTypes = {
   notes: React.PropTypes.array.isRequired
